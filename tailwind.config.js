a90a33b7019ea814be5ed68988340b3ce0d94e1d
Yourengab <yourenw1@gmail.com>
2023-05-02 00:12:34 +0800

    Test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./views/landing-page/index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
