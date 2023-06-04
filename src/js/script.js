8479974480102a6a19d182e70821b78a52a2764c
Yourengab <yourenw1@gmail.com>
2023-06-04 09:27:05 +0800

    Update: add warning

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,7 @@ setInterval(updateTime, 10);
 $(".slider").slick({
   dots: true,
   speed: 300,
-  arrows: true,
+  arrows: false,
   slidesToShow: 3,
   slidesToScroll: 3,
   responsive: [
@@ -63,15 +63,17 @@ function displayBooks(book) {
 }
 
 function mappedBooksInContainer(bookList) {
-  return `<div class="mb-10" id="books-box">
-  <img src="${bookList.volumeInfo.imageLinks[0]}" alt="books" class="bg-green-400 w-64 h-80 rounded-xl" />
+  return `<div class="mb-10 bg-white p-4 rounded-xl shadow-xl" id="books-box">
+  <img src="${bookList.volumeInfo.imageLinks.thumbnail}" alt="books" class="shadow-xl w-64 h-80 rounded-xl" />
   <div class="mt-6">
-    <h2 class="uppercase text-lg font-bold text-main tracking-tight">${bookList.volumeInfo.authors}</h2>
-    <p class="capitalize mb-8 mt-2 text-xl font-semibold text-dark tracking-tight w-64">${bookList.volumeInfo.title}</p>
+    <h2 class="uppercase text-lg font-bold text-main tracking-tight w-64">${bookList.volumeInfo.authors}</h2>
+    <p class="capitalize mb-8 mt-2 text-xl font-semibold text-dark tracking-tight w-64">${
+      bookList.volumeInfo.title.length > 30 ? (bookList.volumeInfo.title = bookList.volumeInfo.title.substring(0, 30) + "...") : bookList.volumeInfo.title
+    }</p>
     <span class="py-2 px-4 bg-gray-300 rounded-full max-w-lg text-dark tracking-tight">Genre & Agak Panjang</span>
-    <div class="flex gap-6 mt-10">
-      <button class="px-4 bg-main rounded-full text-white h-12 font-medium">Synopsis</button>
-      <button class="px-4 h-12 rounded-full text-dark font-medium">Save to library <i class="fa-solid fa-bookmark"></i></button>
+    <div class="flex gap-6 justify-between mt-10">
+      <button class="w-full bg-main rounded-full text-white h-12 font-medium">Synopsis</button>
+      <button class="border px-4 border-dark  rounded-fullh-12 rounded-full text-dark font-medium"><i class="fa-solid fa-bookmark"></i></button>
     </div>
   </div>
 </div>`;
