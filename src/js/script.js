d5145362f70e8be17c36cc45fd3e0aca39aed48f
Yourengab <yourenw1@gmail.com>
2023-06-05 12:16:52 +0800

    New Refaractor

--- src/js/script.js
+++ src/js/script.js
@@ -20,10 +20,15 @@ searchBtn.addEventListener("click", async function () {
 });
 
 // *********************** GET BOOKS *********************** \\
-function getBooks(keyword) {
-  return fetch("https://www.googleapis.com/books/v1/volumes?q=" + keyword + "&key=AIzaSyD952R5dTCubXZWsul-S7TiGwzsY9IF0uw")
-    .then((response) => response.json())
-    .then((response) => response.items);
+async function getBooks(keyword) {
+  try {
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${keyword}&key=AIzaSyD952R5dTCubXZWsul-S7TiGwzsY9IF0uw`);
+    const data = await response.json();
+    return data.items;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
 
 // *********************** DISPLAY BOOKS TO HTML *********************** \\
@@ -37,16 +42,17 @@ function displayBooks(book) {
 
 // *********************** MAPPED BOOKS TO CARD *********************** \\
 function mappedBooksInContainer(bookList) {
+  const { volumeInfo } = bookList;
+  const truncatedTitle = volumeInfo.title.length > 30 ? volumeInfo.title.substring(0, 30) + "..." : volumeInfo.title;
+
   return `<div class="mb-10 bg-white p-4 rounded-xl shadow-xl" id="books-box">
-  <img src="${bookList.volumeInfo.imageLinks.thumbnail}" alt="books" class="shadow-xl w-64 h-80 rounded-xl" />
+  <img src="${volumeInfo.imageLinks.thumbnail}" alt="books" class="shadow-xl w-64 h-80 rounded-xl" />
   
   <div class="mt-6">
-    <h2 class="uppercase text-lg font-bold text-main tracking-tight w-64">${bookList.volumeInfo.authors}</h2>
-    <p class="capitalize mb-8 mt-2 text-xl font-semibold text-dark tracking-tight w-64">${
-      bookList.volumeInfo.title.length > 30 ? (bookList.volumeInfo.title = bookList.volumeInfo.title.substring(0, 30) + "...") : bookList.volumeInfo.title
-    }</p>
-    <span class="py-2 px-4 bg-gray-300 rounded-full max-w-lg text-dark tracking-tight">${!bookList.volumeInfo.categories ? "Unknown Categories" : bookList.volumeInfo.categories[0].split(",")[0]}</span>
-    
+    <h2 class="uppercase text-lg font-bold text-main tracking-tight w-64">${volumeInfo.authors}</h2>
+    <p class="capitalize mb-8 mt-2 text-xl font-semibold text-dark tracking-tight w-64">${truncatedTitle}</p>
+    <span class="py-2 px-4 bg-gray-300 rounded-full max-w-lg text-dark tracking-tight">${!volumeInfo.categories ? "Unknown Categories" : volumeInfo.categories[0].split(",")[0]}</span>
+
     <div class="flex gap-6 justify-between mt-10">
       <button class="w-full bg-main rounded-full text-white h-12 font-medium">Synopsis</button>
       <button class="border px-4 border-dark  rounded-fullh-12 rounded-full text-dark font-medium"><i class="fa-solid fa-bookmark"></i></button>
