8f86fa5d852a9d7c6e8619eb354c722645df57df
anggaraptra <kadekanggaraptra@gmail.com>
2023-06-07 01:42:27 +0800

    add connection db

--- functions/functions.php
+++ functions/functions.php
@@ -1,2 +1,16 @@
-<?php 
-// function connect db
+<?php
+// ********************* Database Connection ********************* //
+$user = "root";
+$pass = "";
+$host = "localhost";
+$db = "db_millobooks";
+
+// Create connection
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// ********************* Manage Query ********************* //
