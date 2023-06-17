ab5c50e60f594284e596e9f219f1ff46ab3dc852
anggaraptra <kadekanggaraptra@gmail.com>
2023-06-17 20:52:51 +0800

    add database query

--- functions/functions.php
+++ functions/functions.php
@@ -14,3 +14,13 @@ if (!$conn) {
 }
 
 // ********************* Manage Query ********************* //
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
